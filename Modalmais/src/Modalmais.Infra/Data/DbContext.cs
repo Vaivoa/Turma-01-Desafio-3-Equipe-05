a53d769e68b7af3c5863fa417162ebd87e784021
LucasTMP <50775822+LucasTMP@users.noreply.github.com>
2021-08-19 13:35:25 -0300

    feat(dbcontext): implementação da conexao com o MongoDb

---
+++ Modalmais/src/Modalmais.Infra/Data/DbContext.cs
@@ -0,0 +1,44 @@
+﻿using Modalmais.Business.Models;
+using MongoDB.Driver;
+
+namespace Modalmais.Infra.Data
+{
+    public class DbContext
+    {
+        const string StringConnection = "mongodb://localhost:27017";
+        const string Db = "Modalmais";
+
+        private static readonly IMongoClient _client;
+        private static readonly IMongoDatabase _dataBase;
+
+
+        //exemplo de uso
+        //var ass = new DbContext(); _context.
+        //ass.Clientes.FindAsync(); ou _context.Clientes.FindAsync();
+
+
+        static DbContext()
+        {
+            _client = new MongoClient(StringConnection);
+            _dataBase = _client.GetDatabase(Db);
+
+        }
+
+        public IMongoClient Client
+        {
+            get
+            {
+                return _client;
+            }
+        }
+
+        public IMongoCollection<Cliente> Clientes
+        {
+            get
+            {
+                return _dataBase.GetCollection<Cliente>("Clientes");
+            }
+        }
+
+    }
+}
