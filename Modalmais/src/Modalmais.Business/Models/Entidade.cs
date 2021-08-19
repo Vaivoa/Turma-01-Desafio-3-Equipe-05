5d9828e69bf97784d89d422de8b4780776c99cdb
Matheus Paixao <matheus.paixao@vaivoa.com>
2021-08-19 10:14:26 -0300

    Criar Entidade Conta Corrente e Cliente

---
+++ Modalmais/src/Modalmais.Business/Models/Entidade.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modalmais.Business.Models
+{
+    public class Entidade
+    {
+        [BsonId]
+        public string Id { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public Entidade()
+        {
+            DataCriacao = DateTime.Now;
+        }
+    }
+}
