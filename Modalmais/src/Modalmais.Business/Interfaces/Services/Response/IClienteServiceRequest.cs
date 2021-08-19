308301b6c17296d628f90e4103b2573464523605
Matheus Pimentel <matheusvsm.pimentel@hotmail.com>
2021-08-19 16:15:14 -0300

    Criado pasta interfaces IClienteServiceRequest e IClienteServiceResponse

---
+++ Modalmais/src/Modalmais.Business/Interfaces/Services/Response/IClienteServiceRequest.cs
@@ -0,0 +1,14 @@
+﻿using Modalmais.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modalmais.Business.Interfaces.Services.Response
+{
+    public interface IClienteServiceRequest
+    {
+        Task AdicionarCliente(Cliente clienteAdicionar);
+    }
+}
