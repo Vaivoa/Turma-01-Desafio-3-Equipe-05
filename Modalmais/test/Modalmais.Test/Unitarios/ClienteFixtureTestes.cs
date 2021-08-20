b8453a25ee0bdaa3be9a2283396a314ae99c4b60
Vitor Jansen <vitorvieira.id@gmail.com>
2021-08-19 22:22:09 -0300

    refactor: gerar cliente válido de modo aleatório com Bogus

--- Modalmais/test/Modalmais.Test/Unitarios/ClienteFixtureTestes.cs
+++ Modalmais/test/Modalmais.Test/Unitarios/ClienteFixtureTestes.cs
@@ -2,6 +2,9 @@
 using Modalmais.Business.Models;
 using Modalmais.Business.Models.Enums;
 using Modalmais.Business.Models.ObjectValues;
+using Bogus;
+using Bogus.DataSets;
+using Bogus.Extensions.Brazil;
 using Xunit;
 
 namespace Modalmais.Test.Unitarios
@@ -14,15 +17,17 @@ namespace Modalmais.Test.Unitarios
     {
         public Cliente GerarClienteValido()
         {
-            var clienteValido = new Cliente(
-                "Ricardo",
-                "Camargo",
-                "48751466040",
-                new Contato(
-                    new Celular(DDDBrasil.AM_Manaus, "789855547"),
-                    "email@email.com"
-                )
-            );
+            var genero = new Faker().PickRandom<Name.Gender>();
+            var ddd = new Faker().PickRandom<DDDBrasil>();
+            var numero = new Faker().Random.Number(900000000, 999999999).ToString();
+
+            var clienteValido = new Faker<Cliente>("pt_BR")
+                .CustomInstantiator(f => new Cliente(
+                    f.Name.FirstName(genero),
+                    f.Name.LastName(genero),
+                    f.Person.Cpf(false),
+                    new Contato(new Celular(ddd, numero), f.Internet.ExampleEmail())
+                ));
 
             return clienteValido;
         }
