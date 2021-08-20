3b17b37e0c5a67f92df8532afaaffe78c12d5a02
Lucas Paes <50775822+LucasTMP@users.noreply.github.com>
2021-08-20 09:26:20 -0300

    Revert "Endpoints de busca adicionados"

--- Modalmais/src/Modalmais.API/Controllers/ContaController.cs
+++ Modalmais/src/Modalmais.API/Controllers/ContaController.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Modalmais.API.DTOs;
 using Modalmais.Business.Models;
-using MongoDB.Bson;
 using MongoDB.Driver;
 using System.Threading.Tasks;
 
@@ -14,46 +13,20 @@ namespace Modalmais.API.Controllers
         public ContaCorrenteController(IMapper mapper) : base(mapper) { }
 
         [HttpPost]
-        public async Task<IActionResult> AdicionarCliente(ClienteRequest clienteRequest)
+        public async Task<IActionResult> Cliente(ClienteRequest clienteRequest)
         {
             IMongoCollection<Cliente> clientes = context.GetCollection<Cliente>("Clientes");
 
             var cliente = _mapper.Map<Cliente>(clienteRequest);
 
-            if (!cliente.ValidarUsuario()) return new BadRequestObjectResult(cliente.ListaDeErros);
+            if (!cliente.ValidarUsuario()) return new BadRequestObjectResult(cliente);
 
             await clientes.InsertOneAsync(cliente);
 
-            return new CreatedResult(nameof(AdicionarCliente), "");
+            return new OkObjectResult("");
 
-        }
-
-        [HttpGet]
-        public async Task<IActionResult> ListaClientes()
-        {
-            IMongoCollection<Cliente> clientes = context.GetCollection<Cliente>("Clientes");
-
-            var ListaClientes = await clientes.Find(new BsonDocument()).ToListAsync();
-
-            return new OkObjectResult(ListaClientes);
-        }
-
-        [HttpGet("{id}")]
-        public async Task<IActionResult> ClienteById(ObjectId id)
-        {
-            IMongoCollection<Cliente> clientes = context.GetCollection<Cliente>("Clientes");
-
-            var filtro = new BsonDocument
-            {
-                { "_id", $"{id}"}
-            };
-
-            var Cliente = await clientes.Find(filtro).FirstOrDefaultAsync();
-
-            if (Cliente != null) return new BadRequestObjectResult("Id não encontrado");
-
-            return new OkObjectResult(Cliente);
         }
     }
+
 
 }
