f61ae4665239957f8376a98115ae6e44d39d5350
victor <victor.magdesian@vaivoa.com>
2021-08-23 09:56:23 -0300

    salvando entidade

---
+++ Modalmais/src/Modalmais.Business/Models/ChavePix.cs
@@ -0,0 +1,8 @@
+﻿namespace Modalmais.Business.Models
+{
+    public class ChavePix
+    {
+        public bool Ativo { get; private set; }
+        public string Valor { get; private set; }
+    }
+}
