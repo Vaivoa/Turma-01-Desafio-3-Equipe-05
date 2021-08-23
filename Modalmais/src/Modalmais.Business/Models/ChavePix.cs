3a11d87e19874e42664bc4756f90ad9e675d1add
Victor Magdesian <victor.magdesian@vaivoa.com>
2021-08-23 10:09:50 -0300

    Entidade ChavePix Criada

--- Modalmais/src/Modalmais.Business/Models/ChavePix.cs
+++ Modalmais/src/Modalmais.Business/Models/ChavePix.cs
@@ -1,8 +1,32 @@
-﻿namespace Modalmais.Business.Models
+﻿using Modalmais.Business.Models.Enums;
+
+namespace Modalmais.Business.Models
 {
     public class ChavePix
     {
-        public bool Ativo { get; private set; }
+        public Status Ativo { get; private set; }
         public string Valor { get; private set; }
+        public TipoChavePix Tipo { get; private set; }
+
+        public ChavePix(string valor, TipoChavePix tipo = TipoChavePix.Aleatoria)
+        {
+
+            Ativo = Status.Inativo;
+            Valor = valor;
+            Tipo = tipo;
+
+        }
+
+        public void AtivarChavePix()
+        {
+            Ativo = Status.Ativo;
+
+        }
+
+        public void DesativarChavePix()
+        {
+            Ativo = Status.Desativado;
+
+        }
     }
 }
