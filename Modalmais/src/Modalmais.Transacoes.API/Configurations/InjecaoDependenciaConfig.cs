70eacad81cceff2c557cc02601bfde5171999fd6
Matheus Paixao <matheus.paixao@vaivoa.com>
2021-09-01 19:30:52 -0300

    Fix Https

--- Modalmais/src/Modalmais.Transacoes.API/Configurations/InjecaoDependenciaConfig.cs
+++ Modalmais/src/Modalmais.Transacoes.API/Configurations/InjecaoDependenciaConfig.cs
@@ -3,7 +3,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Modalmais.Core.Interfaces.Notificador;
 using Modalmais.Core.Notificador;
 using Modalmais.Transacoes.API.Data;
+using Modalmais.Transacoes.API.Refit;
 using Modalmais.Transacoes.API.Repository;
+using Refit;
 
 namespace Modalmais.Transacoes.API.Configurations
 {
@@ -12,6 +14,15 @@ namespace Modalmais.Transacoes.API.Configurations
 
         public static IServiceCollection InjecaoDependencias(this IServiceCollection services, IConfiguration configuration)
         {
+
+            services.AddRefitClient<IContaService>().ConfigureHttpClient(c =>
+            {
+                c.BaseAddress = new System.Uri("http://modalmais.api:80/api/v1");
+                if (Startup.env != "Docker")                
+                    c.BaseAddress = new System.Uri("http://localhost:5001/api/v1");
+                
+                
+            });
             //DbContext
             services.AddScoped<ApiDbContext>();
 
--- Modalmais/src/Modalmais.Transacoes.API/Startup.cs
+++ Modalmais/src/Modalmais.Transacoes.API/Startup.cs
@@ -8,6 +8,7 @@ namespace Modalmais.Transacoes.API
 {
     public class Startup
     {
+        public static string env { get; set; }
         public Startup(IWebHostEnvironment hostEnvironment)
         {
             var builder = new ConfigurationBuilder()
@@ -15,7 +16,7 @@ namespace Modalmais.Transacoes.API
                 .AddJsonFile("appsettings.json", true, true)
                 .AddJsonFile($"appsettings.{hostEnvironment.EnvironmentName}.json", true, true)
                 .AddEnvironmentVariables();
-
+            env = hostEnvironment.EnvironmentName;
             Configuration = builder.Build();
         }
 
