d2504db780ab003652f705bcf333758eb05d64d1
LucasTMP <50775822+LucasTMP@users.noreply.github.com>
2021-09-02 15:52:17 -0300

    feat(Dtos): dtos de transação e edição de cadastro.

---
+++ Modalmais/src/Modalmais.API/DTOs/ClienteAlteracaoRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Modalmais.API.DTOs
+{
+    public class ClienteAlteracaoRequest
+    {
+
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public ContatoAddRequest Contato { get; set; }
+
+    }
+}
---
+++ Modalmais/src/Modalmais.Transacoes.API/DTOs/Validations/ExtratoRequestValidation.cs
@@ -0,0 +1,45 @@
+﻿using FluentValidation;
+using Modalmais.Core.Utils;
+
+namespace Modalmais.Transacoes.API.DTOs.Validations
+{
+    public class ExtratoRequestValidation : AbstractValidator<ExtratoRequest>
+    {
+        public static readonly string CampoNaoPodeSerBrancoOuNulo = "O campo {PropertyName} não pode estar em branco ou ser nulo.";
+        public static readonly string AgenciaInvalida = "As consultas de extratos só podem ser realizadas para a agencia 0001.";
+        public static readonly string SomenteNumeros = "O campo {PropertyName} deve possuir somente numeros.";
+        public static readonly string DataFinalInvalida = "O campo de filtro da data final deve ser maior que a data de inicio.";
+        public static readonly string DataInicioInvalida = "O campo de filtro da data inicial deve ser menor que a data final.";
+
+
+
+        public ExtratoRequestValidation()
+        {
+            RuleFor(extratoRequest => extratoRequest.Agencia)
+               .NotEmpty().WithMessage(CampoNaoPodeSerBrancoOuNulo)
+               .Equal("0001").WithMessage(AgenciaInvalida);
+
+            RuleFor(extratoRequest => extratoRequest.Conta)
+               .NotEmpty().WithMessage(CampoNaoPodeSerBrancoOuNulo)
+               .Must(UtilsDigitosNumericos.SoNumeros).WithMessage(SomenteNumeros);
+
+            RuleFor(extratoRequest => extratoRequest.Periodo.DataFinal)
+                .NotEmpty().WithMessage(CampoNaoPodeSerBrancoOuNulo)
+                .GreaterThan(extratoRequest => extratoRequest.Periodo.DataInicio).WithMessage(DataFinalInvalida);
+
+
+            RuleFor(extratoRequest => extratoRequest.Periodo.DataInicio)
+                .NotEmpty().WithMessage(CampoNaoPodeSerBrancoOuNulo)
+                .LessThan(extratoRequest => extratoRequest.Periodo.DataFinal).WithMessage(DataInicioInvalida);
+
+
+            //RuleFor(extratoRequest => extratoRequest.Periodo.DataInicio)
+            //    .NotEmpty().WithMessage(CampoNaoPodeSerBrancoOuNulo)
+            //    .LessThanOrEqualTo(extratoRequest => (int)(extratoRequest.Periodo.DataFinal
+            //                      - extratoRequest.Periodo.DataInicio).)
+            //    .WithMessage(DataInicioInvalida);
+
+        }
+
+    }
+}
